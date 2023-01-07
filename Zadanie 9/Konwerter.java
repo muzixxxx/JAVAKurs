7e68364fba81bdaa5aad5f3d1f38e8d0f231a836
muzixxxx <94138729+muzixxxx@users.noreply.github.com>
2023-01-07 23:18:42 +0100

    Add files via upload

---
+++ Zadanie 9/Konwerter.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Konwerter {
+    private JTextField kursTextField;
+    private JButton przeliczButton;
+    private JTextField kwota1TextField;
+    private JTextField kwota2TextField;
+    private JPanel Main;
+    private JTextField NazwaW;
+    double kwota1;
+    double kwota2;
+    double kurs;
+
+
+    public Konwerter() {
+        przeliczButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                kwota2TextField.setText(String.valueOf((Double.parseDouble(kwota1TextField.getText())*Double.parseDouble(kursTextField.getText()))));
+                kwota2TextField.setText(kwota2TextField.getText()+" "+NazwaW.getText());
+
+                JOptionPane.showMessageDialog(null,("Kwota po przeliczeniu z "+ kwota1TextField.getText()+" PLN to \n"+ kwota2TextField.getText()));
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Konwerter");
+        frame.setContentPane(new Konwerter().Main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
